97d30bd4b460a8b4de3a3cce44591febbc603a3f
Ruan Figuereido <huanfiguereido@gmail.com>
2023-04-04 22:43:27 -0300

    main app
    lista de tarefas desenvolvido com html, css e js

---
+++ README.md
@@ -0,0 +1,2 @@
+# to-do
+ lista de tarefas
---
+++ assets/script/script.js
@@ -0,0 +1,45 @@
+let tarefa = document.querySelector("input");
+let btnAdd = document.querySelector(".input-data button");
+let listaTarefas = [];
+btnAdd.addEventListener("click", adicionarTarefa);
+//ADICIONAR TAREFA
+function adicionarTarefa(){
+	let listaDeTarefas = document.querySelector(".data");
+//CASO JÁ TENHA A TAREFA, EXIBA MENSAGEM "JÁ TEM TAREFA"
+	if(listaTarefas.includes(tarefa.value)){
+		alert('Esta tarefa já está na lista!');
+		tarefa.value="";
+	}
+//CASO NÃO TENHA A TAREFA, ADICIONE TAREFA
+	else{
+//adição da tarefa e exibição da mesma no console
+		listaTarefas.push(tarefa.value);
+//criando a variavel ul, pai de li(tarefa);
+		let exibicaoTarefa = document.querySelector(".data");
+//apadrinhando LI a UL 
+		let novaTarefa = document.createElement("li");
+		exibicaoTarefa.appendChild(novaTarefa);
+		novaTarefa.innerHTML=tarefa.value;
+//apadrinhando btnRemover a LI
+		let btnRemover = document.createElement("button");
+		novaTarefa.appendChild(btnRemover);
+//apadrinhando span a btnRemover
+		let icon = document.createElement("span");
+		btnRemover.appendChild(icon);
+		icon.className="fa-solid fa-trash";
+		tarefa.value="";
+		if(listaTarefas.length>15){
+			let field = document.querySelector(".container");
+			field.style.overflowY="scroll";
+		}
+		btnRemover.addEventListener("click", removeTarefa);
+//FUNÇÃO QUE REMOVE A TAREFA DO DOM
+		function removeTarefa(){
+			novaTarefa.parentNode.removeChild(novaTarefa);
+		}
+	}
+}
+
+
+
+
